6365881bd085768ea208fe58f23eb1c81a6dde21
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-11-02 21:49:05 +0200

    Create Model.py

---
+++ Model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+
+# In[2]:
+
+
+df = pd.read_csv('penguins.csv')
+df
+
