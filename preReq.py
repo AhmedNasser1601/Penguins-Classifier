93bccca8723a44b76202494d48f220c33e422f0c
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-11-05 05:35:36 +0200

    Prerequisites file
    Global Data

---
+++ preReq.py
@@ -0,0 +1,4 @@
+Features = ['bill_length_mm','bill_depth_mm','flipper_length_mm','gender','body_mass_g']
+Classes = ['Adelie','Gentoo','Chinstrap']
+INarr = [' ']*7
+OUTarr = [' ', ' ', ' ', [' ', ' ', ' ', ' '], ' ']
