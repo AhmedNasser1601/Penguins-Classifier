a6fd8fbdde42e8b264d4c43249465f810862dd5b
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-11-07 17:51:22 +0200

    Update preReq.py

--- preReq.py
+++ preReq.py
@@ -1,5 +1,5 @@
-Features = ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'gender', 'body_mass_g']
-Classes = ['Adelie', 'Gentoo', 'Chinstrap']
+Features = x1, x2, x3, x4, x5 = ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'gender', 'body_mass_g']
+Classes = c1, c2, c3 = ['Adelie', 'Gentoo', 'Chinstrap']
 
-INarr = ['bill_length_mm', 'body_mass_g', 'Gentoo', 'Chinstrap', 50, 25, 1]
+INarr = [x1, x5, c2, c3, 50, 25, 1]
 OUTarr = ['test', 0, 0, [41, 42, 43, 44], 'Plotting']
