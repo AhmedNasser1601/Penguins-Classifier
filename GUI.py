1fc2b53ce5822125b099a8acee4eb257bca4136a
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-11-07 17:51:34 +0200

    Final Update
    Model & GUI

--- GUI.py
+++ GUI.py
@@ -1,12 +1,10 @@
 import os
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
 from tkinter import Canvas
+from tkinter import messagebox
+from tkinter import ttk
 
 import preReq
-
-
 
 # Input Part
 Top = tk.Tk()
@@ -20,7 +18,6 @@ Input_lbl.place(x=130, y=1)
 Output_lbl = tk.Label(Top, text='Output', font=('Times New Roman', 27), foreground="#a52a2a")
 Output_lbl.place(x=630, y=1)
 
-
 Feature1_lbl = tk.Label(Top, text='Select 1st Feature', font=('Verdana', 12), foreground="Blue")
 Feature1_lbl.place(x=10, y=60)
 
@@ -28,7 +25,6 @@ Feature1 = ttk.Combobox(Top, width=15, font=('Verdana', 10))
 Feature1['values'] = preReq.Features
 Feature1.current()
 Feature1.place(x=200, y=60)
-
 
 Feature2_lbl = tk.Label(Top, text='Select 2nd Feature', font=('Verdana', 12), foreground="Blue")
 Feature2_lbl.place(x=10, y=100)
@@ -38,7 +34,6 @@ Feature2['values'] = preReq.Features
 Feature2.current()
 Feature2.place(x=200, y=100)
 
-
 Class1_lbl = tk.Label(Top, text='Select 1st Class', font=('Verdana', 12), foreground="Blue")
 Class1_lbl.place(x=10, y=160)
 
@@ -47,39 +42,37 @@ Class1['values'] = preReq.Classes
 Class1.current()
 Class1.place(x=200, y=160)
 
-
-Class2_lbl = tk.Label(Top,text='Select 2nd Class', font=('Verdana', 12),foreground="Blue")
+Class2_lbl = tk.Label(Top, text='Select 2nd Class', font=('Verdana', 12), foreground="Blue")
 Class2_lbl.place(x=10, y=200)
 
-Class2 = ttk.Combobox(Top,width=15, font=('Verdana', 10))
+Class2 = ttk.Combobox(Top, width=15, font=('Verdana', 10))
 Class2['values'] = preReq.Classes
 Class2.current()
 Class2.place(x=200, y=200)
-
 
 eta_lbl = tk.Label(Top, text='Enter eta value', font=('Verdana', 12), foreground="Blue")
 eta_lbl.place(x=10, y=260)
 
 eta = tk.Entry(Top, font=('Verdana', 10), foreground="Blue", width=10)
-eta.place(x =200, y=260)
+eta.place(x=200, y=260)
 
-
-ebochs_lbl = tk.Label(Top,text='Enter no. of ebochs', font=('Verdana', 12), foreground="Blue")
+ebochs_lbl = tk.Label(Top, text='Enter no. of ebochs', font=('Verdana', 12), foreground="Blue")
 ebochs_lbl.place(x=10, y=300)
 
 ebochs = tk.Entry(Top, font=('Verdana', 10), foreground="Blue", width=10)
 ebochs.place(x=200, y=300)
 
-
 CheckBias = tk.IntVar()
-Bias = tk.Checkbutton(Top, text="Bias", variable=CheckBias, onvalue=1, offvalue=0, font=('Verdana', 20, 'bold'), foreground="Orange")
+Bias = tk.Checkbutton(Top, text="Bias", variable=CheckBias, onvalue=1, offvalue=0, font=('Verdana', 20, 'bold'),
+                      foreground="Orange")
 Bias.place(x=50, y=360)
 
 
 def PredictFn():
-    if (Feature1.get()=='') or (Feature2.get()=='') or (Class1.get()=='') or (Class2.get()=='') or (eta.get()=='') or (ebochs.get()==''):
+    if (Feature1.get() == '') or (Feature2.get() == '') or (Class1.get() == '') or (Class2.get() == '') or (
+            eta.get() == '') or (ebochs.get() == ''):
         messagebox.showerror(title="error", message="Insert the missing inputs", parent=Top)
-    elif (Feature1.get()==Feature2.get()) or (Class1.get()==Class2.get()):
+    elif (Feature1.get() == Feature2.get()) or (Class1.get() == Class2.get()):
         messagebox.showerror(title="error", message="Don't reapet your choose", parent=Top)
     else:
         preReq.INarr[0] = Feature1.get()
@@ -91,14 +84,17 @@ def PredictFn():
         preReq.INarr[6] = CheckBias.get()
         exec(open('Model.py').read())
         Top.destroy()
-    
+
+
 def ExitFn():
     os._exit(1)
+
 
 ExitBTN = tk.Button(Top, text="Exit", font=('Verdana', 15, 'bold'), bg="red", foreground="yellow", command=ExitFn)
 ExitBTN.place(x=90, y=430)
 
-PredictBTN = tk.Button(Top, text="Run", font=('Verdana', 15, 'bold'), bg="green", foreground="yellow", command=PredictFn)
+PredictBTN = tk.Button(Top, text="Run", font=('Verdana', 15, 'bold'), bg="green", foreground="yellow",
+                       command=PredictFn)
 PredictBTN.place(x=210, y=430)
 
 canvas = Canvas(Top, width=5, height=500)
@@ -107,19 +103,16 @@ for x in range(5):
 canvas.place(x=380, y=0)
 Top.mainloop()
 
-
 Top = tk.Tk()
 Top.title("Penguins-Classifier-Perceptron")
 Top.geometry('1000x500')
 Top.resizable(0, 0)
-
 
 Input_lbl = tk.Label(Top, text='Input', font=('Times New Roman', 27), foreground="#a52a2a")
 Input_lbl.place(x=130, y=1)
 
 Output_lbl = tk.Label(Top, text='Output', font=('Times New Roman', 27), foreground="#a52a2a")
 Output_lbl.place(x=630, y=1)
-
 
 Feature1_lbl = tk.Label(Top, text='Select 1st Feature', font=('Verdana', 12), foreground="Blue")
 Feature1_lbl.place(x=10, y=60)
@@ -130,7 +123,6 @@ Feature1.insert('end', preReq.INarr[0])
 Feature1.configure(state='disabled')
 Feature1.place(x=200, y=60)
 
-
 Feature2_lbl = tk.Label(Top, text='Select 2nd Feature', font=('Verdana', 12), foreground="Blue")
 Feature2_lbl.place(x=10, y=100)
 
@@ -139,7 +131,6 @@ Feature2.configure(state='normal')
 Feature2.insert('end', preReq.INarr[1])
 Feature2.configure(state='disabled')
 Feature2.place(x=200, y=100)
-
 
 Class1_lbl = tk.Label(Top, text='Select 1st Class', font=('Verdana', 12), foreground="Blue")
 Class1_lbl.place(x=10, y=160)
@@ -150,8 +141,7 @@ Class1.insert('end', preReq.INarr[2])
 Class1.configure(state='disabled')
 Class1.place(x=200, y=160)
 
-
-Class2_lbl = tk.Label(Top,text='Select 2nd Class', font=('Verdana', 12),foreground="Blue")
+Class2_lbl = tk.Label(Top, text='Select 2nd Class', font=('Verdana', 12), foreground="Blue")
 Class2_lbl.place(x=10, y=200)
 
 Class2 = tk.Entry(Top, font=('Verdana', 12), foreground="green", width=8)
@@ -160,7 +150,6 @@ Class2.insert('end', preReq.INarr[3])
 Class2.configure(state='disabled')
 Class2.place(x=200, y=200)
 
-
 eta_lbl = tk.Label(Top, text='Enter eta value', font=('Verdana', 12), foreground="Blue")
 eta_lbl.place(x=10, y=260)
 
@@ -168,10 +157,9 @@ eta = tk.Entry(Top, font=('Verdana', 12), foreground="Blue", width=8)
 eta.configure(state='normal')
 eta.insert('end', preReq.INarr[4])
 eta.configure(state='disabled')
-eta.place(x =200, y=260)
+eta.place(x=200, y=260)
 
-
-ebochs_lbl = tk.Label(Top,text='Enter no. of ebochs', font=('Verdana', 12), foreground="Blue")
+ebochs_lbl = tk.Label(Top, text='Enter no. of ebochs', font=('Verdana', 12), foreground="Blue")
 ebochs_lbl.place(x=10, y=300)
 
 ebochs = tk.Entry(Top, font=('Verdana', 12), foreground="Blue", width=8)
@@ -180,24 +168,21 @@ ebochs.insert('end', preReq.INarr[5])
 ebochs.configure(state='disabled')
 ebochs.place(x=200, y=300)
 
-
 CheckBias = tk.IntVar()
-Bias = tk.Checkbutton(Top, text="Bias", variable=CheckBias, onvalue=1, offvalue=0, font=('Verdana', 20, 'bold'), foreground="Orange")
+Bias = tk.Checkbutton(Top, text="Bias", variable=CheckBias, onvalue=1, offvalue=0, font=('Verdana', 20, 'bold'),
+                      foreground="Orange")
 if preReq.INarr[6]:
     Bias.select()
 Bias.configure(state='disabled')
 Bias.place(x=50, y=360)
 
-
 ExitBTN = tk.Button(Top, text="Exit", font=('Verdana', 15, 'bold'), bg="red", foreground="yellow", command=ExitFn)
 ExitBTN.place(x=150, y=430)
-
 
 canvas = Canvas(Top, width=5, height=500)
 for x in range(5):
     canvas.create_line(x, 0, x, 500, fill="#a52a2a")
 canvas.place(x=380, y=0)
-
 
 pClass_lbl = tk.Label(Top, text='Predicted :', font=('Verdana', 12), foreground="orange")
 pClass_lbl.place(x=400, y=100)
@@ -208,7 +193,6 @@ pClass.insert('end', preReq.OUTarr[0])
 pClass.configure(state='disabled')
 pClass.place(x=500, y=100)
 
-
 trainAcc_lbl = tk.Label(Top, text='Train Acc :', font=('Verdana', 12), foreground="orange")
 trainAcc_lbl.place(x=400, y=160)
 
@@ -218,7 +202,6 @@ trainAcc.insert('end', (preReq.OUTarr[1], '%'))
 trainAcc.configure(state='disabled')
 trainAcc.place(x=500, y=160)
 
-
 testAcc_lbl = tk.Label(Top, text='Test  Acc : ', font=('Verdana', 12), foreground="orange")
 testAcc_lbl.place(x=400, y=200)
 
@@ -227,7 +210,6 @@ testAcc.configure(state='normal')
 testAcc.insert('end', (preReq.OUTarr[2], '%'))
 testAcc.configure(state='disabled')
 testAcc.place(x=500, y=200)
-
 
 confMat_lbl = tk.Label(Top, text='Confusion\nMatrix', font=('Verdana', 12), foreground="orange")
 confMat_lbl.place(x=400, y=260)
--- Model.py
+++ Model.py
@@ -1,15 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
 from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
+
 import preReq
-
-
 
 df = pd.read_csv('penguins.csv')
 df['gender'] = LabelEncoder().fit_transform(df['gender'])
-scaled_df = pd.DataFrame(preprocessing.MinMaxScaler().fit_transform(df.iloc[:,1:]), columns=df.iloc[:,1:].columns)
+scaled_df = pd.DataFrame(preprocessing.MinMaxScaler().fit_transform(df.iloc[:, 1:]), columns=df.iloc[:, 1:].columns)
 scaled_df['species'] = df['species']
 
 selectedF = (preReq.INarr[0], preReq.INarr[1])
@@ -22,11 +21,11 @@ bias = np.random.randn() if preReq.INarr[6] else 0
 
 # Visualizations
 def visualizeData(scaled_df):  # 10 combs
-    for i in range(len(preReq.Features)-1):
-        for j in range(i+1, len(preReq.Features)):
+    for i in range(len(preReq.Features) - 1):
+        for j in range(i + 1, len(preReq.Features)):
             fL1, fL2 = preReq.Features[i], preReq.Features[j]
             f1, f2 = scaled_df[fL1], scaled_df[fL2]
-            
+
             plt.xlabel(fL1)
             plt.ylabel(fL2)
 
@@ -36,19 +35,28 @@ def visualizeData(scaled_df):  # 10 combs
 
             plt.legend([preReq.Classes[0], preReq.Classes[1], preReq.Classes[2]])
             plt.show()
-            
-#visualizeData(scaled_df)
+
+
+# visualizeData(scaled_df)
 
 
 # Main
 
-misClass = [x for x in preReq.Classes if x not in selectedC]
-misFeatures = [x for x in preReq.Features if x not in selectedF]
+misClass = list()
+for x in preReq.Classes:
+    if x not in selectedC:
+        misClass.append(x)
+# misClass = [x for x in preReq.Classes if x not in selectedC]
+
+misFeatures = list()
+for x in preReq.Features:
+    if x not in selectedF:
+        misFeatures.append(x)
+# misFeatures = [x for x in preReq.Features if x not in selectedF]
 
 scaled_df.drop(scaled_df.index[(scaled_df["species"] == misClass[0])], axis=0, inplace=True)
 scaled_df.drop(columns=misFeatures, axis=1, inplace=True)
 scaled_df['species'] = scaled_df['species'].replace(selectedC, [1, -1])
-
 
 C1, C2 = (scaled_df[:50]).sample(frac=1), (scaled_df[50:100]).sample(frac=1)
 C1_train, C1_test, C2_train, C2_test = C1[:30], C1[30:], C2[:30], C2[30:]
@@ -56,42 +64,46 @@ C1_train, C1_test, C2_train, C2_test = C1[:30], C1[30:], C2[:30], C2[30:]
 all_train_data = pd.concat([C1_train, C2_train]).to_numpy()
 all_test_data = pd.concat([C1_test, C2_test]).to_numpy()
 
-train_data, train_target = all_train_data[:,:2], all_train_data[:, 2:]
-test_data, test_target = all_test_data[:,:2], all_test_data[:, 2:]
+train_data, train_target = all_train_data[:, :2], all_train_data[:, 2:]
+test_data, test_target = all_test_data[:, :2], all_test_data[:, 2:]
 
 
+def PerceptronAlgo(epochs, weight, bias, eta, train_data, train_target):
+    import numpy as np
 
-
-def PerceptronAlgo(weight, bias):
     for epoch in range(epochs):
         predict = (train_data.dot(weight.transpose()) + bias)
-        yPredTrain = np.where(predict>0, 1, -1)
+        yPredTrain = np.where(predict > 0, 1, -1)
         for i in range(len(train_target)):
             if yPredTrain[i] != train_target[i]:
                 loss = train_target[i] - yPredTrain[i]
-                weight += eta*loss*train_data[i]
-                bias += eta*loss if bias else 0
+                weight += (eta * loss * train_data[i])
+                bias += (eta * loss) if bias else 0
     return yPredTrain
+
 
 def classifyTrain():
     plt.figure("Trained Features Figure")
     plt.scatter(x=train_data[:, :1], y=train_data[:, 1:2], c=train_target)
-    plt.plot(np.array([0, 1]), np.array([(-bias)/weight[1], (-weight[0]-bias)/weight[1]]))
+    plt.plot(np.array([0, 1]), np.array([(-bias) / weight[1], (-weight[0] - bias) / weight[1]]))
     plt.show()
 
-yPredTrain = PerceptronAlgo(weight, bias)
-classifyTrain()
 
+# classifyTrain()
 
+yPredTrain = PerceptronAlgo(epochs, weight, bias, eta, train_data, train_target)
 
 # Testing
-ytest=(test_data.dot(weight.transpose()) + bias)
-yPredTest = np.where(ytest>0, 1, -1)
+ytest = (test_data.dot(weight.transpose()) + bias)
+yPredTest = np.where(ytest > 0, 1, -1)
+
 
 def ConfuionMatrix(target, yPred):
+    import numpy as np
+
     row = list()
     confMat = np.zeros([2, 2])
-    
+
     for i in range(len(yPred)):
         if target[i] == 1:
             if yPred[i] == 1:
@@ -109,17 +121,18 @@ def ConfuionMatrix(target, yPred):
                 confMat[1][0] += 1
                 row.append([target[i], yPred[i], 'F'])
 
-    return ((np.trace(confMat)/np.sum(confMat))*100), confMat
+    return ((np.trace(confMat) / np.sum(confMat)) * 100), confMat
 
-trainAcc, confMat = ConfuionMatrix(train_target, yPredTrain)
-testAcc, confMat = ConfuionMatrix(test_target, yPredTest)
-print("Training",trainAcc)
-print("Testing",testAcc)
+
+trainAcc, confMatTrain = ConfuionMatrix(train_target, yPredTrain)
+testAcc, confMatTest = ConfuionMatrix(test_target, yPredTest)
+print("Training", trainAcc)
+print("Testing", testAcc)
 
 preReq.OUTarr[0] = selectedC[0] if sum(yPredTest) else selectedC[1]
 preReq.OUTarr[1] = trainAcc
 preReq.OUTarr[2] = testAcc
-preReq.OUTarr[3][0] = confMat[0][0]
-preReq.OUTarr[3][1] = confMat[0][1]
-preReq.OUTarr[3][2] = confMat[1][0]
-preReq.OUTarr[3][3] = confMat[1][1]
+preReq.OUTarr[3][0] = confMatTest[0][0]
+preReq.OUTarr[3][1] = confMatTest[0][1]
+preReq.OUTarr[3][2] = confMatTest[1][0]
+preReq.OUTarr[3][3] = confMatTest[1][1]
