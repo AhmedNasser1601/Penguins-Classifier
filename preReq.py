8f530f7f2919145ad92abe3313116e575c691e79
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-11-07 20:11:49 +0200

    Revert "Update preReq.py"
    This reverts commit a1204aa90c094258e0116124c6caa4fd4a5bd968.

--- preReq.py
+++ preReq.py
@@ -1,10 +1,5 @@
-import numpy as np
-
 Features = x1, x2, x3, x4, x5 = ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'gender', 'body_mass_g']
 Classes = c1, c2, c3 = ['Adelie', 'Gentoo', 'Chinstrap']
-
-train_data, target_data = np, np
-weight, bias = 0, 0
 
 INarr = [x1, x5, c2, c3, 50, 25, 1]
 OUTarr = ['test', 0, 0, [0, 0, 0, 0], 'Plotting']
