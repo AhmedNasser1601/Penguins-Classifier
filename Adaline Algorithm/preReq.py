0b0251b132cfbe85af5d725e45eaf8f04f923b16
YossefEFM <87450165+YossefEFM@users.noreply.github.com>
2022-11-17 21:36:23 +0200

    First upload

---
+++ Adaline Algorithm/preReq.py
@@ -0,0 +1,5 @@
+Features = x1, x2, x3, x4, x5 = ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'gender', 'body_mass_g']
+Classes = c1, c2, c3 = ['Adelie', 'Gentoo', 'Chinstrap']
+
+INarr = [x1, x5, c2, c3, 50, 25, 1,50]
+OUTarr = ['test', 0, 0, [0, 0, 0, 0], 'Plotting']
