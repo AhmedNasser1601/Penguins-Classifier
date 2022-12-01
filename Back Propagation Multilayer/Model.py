ad9a1386d90b3e703b2b4e1df0cab6bc607f5215
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-12-01 23:17:10 +0200

    Update Model.py

--- Back Propagation Multilayer/Model.py
+++ Back Propagation Multilayer/Model.py
@@ -3,17 +3,16 @@ import pandas as pd
 import warnings
 
 
-def activation(val, activeFn):
+def activationFn(val, activeFn):
     if activeFn == 'Sigmoid':
         warnings.filterwarnings('ignore')
         return 1/(1 + np.exp(-val))
     else:
+        warnings.filterwarnings('ignore')
         return np.tanh(val)
 
 
-def BackPropagationAlgo(inTrain, inTest, outTrain, weights, activeFn, epochs, eta, layers, nn, bias):
-    import numpy as np
-
+def BackPropagationFn(inTrain, inTest, outTrain, weights, activeFn, epochs, eta, layers, nn, bias):
     neurons = list()
     for n in nn:
         neurons.append(np.zeros(n))
@@ -25,9 +24,9 @@ def BackPropagationAlgo(inTrain, inTest, outTrain, weights, activeFn, epochs, et
         for itr in range(len(inTrain)):
             for layer in range(layers+1):  #Feed Forward
                 if layer == 0:
-                    neurons[0] = activation(inTrain[itr].dot(weights[0]), activeFn)
+                    neurons[0] = activationFn(inTrain[itr].dot(weights[0]), activeFn)
                     continue
-                neurons[layer] = activation(neurons[layer-1].dot(weights[layer]), activeFn)
+                neurons[layer] = activationFn(neurons[layer-1].dot(weights[layer]), activeFn)
             for n in range(len(neurons)-1):
                 neurons[n][0] = bias
             
@@ -48,44 +47,44 @@ def BackPropagationAlgo(inTrain, inTest, outTrain, weights, activeFn, epochs, et
                 inNew = neurons[layer-1].reshape(x, 1).dot(holder[layer].reshape(1, y))
                 weights[layer] += (eta * inNew)
     
-    # Testing & Predict Output
-    fOut = list()
-    for i in range(len(inTest)):
-        for layer in range(layers+1):  #Feed Forward
-            if layer == 0:
-                final[layer] = activation(inTest[i].dot(weights[layer]), activeFn)
-                continue
-            final[layer] = activation(final[layer-1].dot(weights[layer]), activeFn)
-        for n in range(len(final)-1):
-            final[n][0] = bias
-        fOut.append(final[-1])
+    outPred = list()
+    for input in (inTrain, inTest):
+        fOut = list()
+        for i in range(len(input)):
+            for layer in range(layers+1):  #Feed Forward
+                if layer == 0:
+                    final[layer] = activationFn(input[i].dot(weights[layer]), activeFn)
+                    continue
+                final[layer] = activationFn(final[layer-1].dot(weights[layer]), activeFn)
+            for n in range(len(final)-1):
+                final[n][0] = bias
+            fOut.append(final[-1])
 
-    for y in fOut:
-        maximum = np.argmax(y)
-        for idx in range(len(y)):
-            y[idx] = 1 if idx==maximum else 0
-    
-    return fOut
+        for y in fOut:
+            maximum = np.argmax(y)
+            for idx in range(len(y)):
+                y[idx] = 1 if idx==maximum else 0
+        outPred.append(fOut)
+    return outPred
 
 
-def ConfusionMatrix(targetTest, predOut):
+def ConfusionMatrixFn(target, predOut):
     case = list()
     confMat = np.zeros([3, 3])
     classes = ['Adelie', 'Gentoo', 'Chinstrap']
     truthVals = pd.DataFrame(columns=['Pred', 'Real', 'Match'])
 
-    for idx in range(len(targetTest)):
-        confMat[np.argmax(targetTest[idx])][np.argmax(predOut[idx])] += 1
+    for idx in range(len(target)):
+        confMat[np.argmax(target[idx])][np.argmax(predOut[idx])] += 1
         case.append([
             classes[np.argmax(predOut[idx])],
-            classes[np.argmax(targetTest[idx])],
-            '[*]' if np.argmax(targetTest[idx])==np.argmax(predOut[idx]) else '[ ]']
+            classes[np.argmax(target[idx])],
+            '[*]' if np.argmax(target[idx])==np.argmax(predOut[idx]) else '[ ]']
         )
     
     truthVals = truthVals.append(pd.DataFrame(case, columns=['Pred', 'Real', 'Match']), ignore_index=True)
     
     print(
-        '\n   |> Testing Evaluation <| \n',
         '    Overall Acc: %{0:.2f}'.format((np.trace(confMat)/np.sum(confMat))*100),
         '\n\n', truthVals,
         '\n\n', '-----> Confusion Matrix <-----\n',
@@ -93,5 +92,4 @@ def ConfusionMatrix(targetTest, predOut):
     )
 
     for i in range(len(classes)):
-        print('{}: %{}'.format(classes[i], ((confMat[i][i]/(len(targetTest)/len(classes)))*100)))
-    print('\n', '-'*50)
+        print('{}: %{}'.format(classes[i], ((confMat[i][i]/(len(target)/len(classes)))*100)))
